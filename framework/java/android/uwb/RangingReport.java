1d6fbe77cb9e8222939592b37708a333f9111a56
Shreshta Manu <shreshtabm@google.com>
2022-11-08 06:31:16 +0000

    DO NOT MERGE: (uwb) Hide oem extension api in tm-mainline-prod
    Hiding oem extension api in tm-mainline-prod branch, only exposing
    it in master branch now.
    
    Bug: 243555651
    Test: compiles
    Change-Id: I5d91b7cd91738652358564eabe7de9fc00f30320
    (cherry picked from commit d71609510ad7345b5d40685b13dc8c5182856e55)
    Merged-In: I5d91b7cd91738652358564eabe7de9fc00f30320

--- framework/java/android/uwb/RangingReport.java
+++ framework/java/android/uwb/RangingReport.java
@@ -60,7 +60,7 @@ public final class RangingReport implements Parcelable {
 
     /**
      * Gets ranging report metadata passed by vendor
-     *
+     * @hide
      * @return vendor data for ranging report
      */
     @NonNull
@@ -161,7 +161,7 @@ public final class RangingReport implements Parcelable {
 
         /**
          * Add ranging report metadata
-         *
+         * @hide
          * @param rangingReportMetadata vendor data per ranging report
          *
          * @throws IllegalStateException if rangingReportMetadata is null
