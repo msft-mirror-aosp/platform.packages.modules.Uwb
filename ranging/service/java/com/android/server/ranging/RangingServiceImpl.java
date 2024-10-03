bdb50659113f982dccb14918662936e7a7d17710
Shreshta Manu <shreshtabm@google.com>
2024-10-03 20:05:23 +0000

    [Ranging] Enforce RANGING permission pre flight for start ranging
    Bug: 370977414
    Test: Compiles
    Flag: com.android.ranging.flags.ranging_stack_enabled
    Relnote: Add new permission for ranging
    Change-Id: Ib9580b130964319086cac3fc7e6ddc27771411bc

--- ranging/service/java/com/android/server/ranging/RangingServiceImpl.java
+++ ranging/service/java/com/android/server/ranging/RangingServiceImpl.java
@@ -44,6 +44,7 @@ public class RangingServiceImpl extends IRangingAdapter.Stub {
     @Override
     public void startRanging(AttributionSource attributionSource, SessionHandle sessionHandle,
             RangingPreference rangingPreference, IRangingCallbacks callbacks) {
+        mRangingInjector.enforceRangingPermissionForPreflight(attributionSource);
         mRangingInjector.getRangingServiceManager().startRanging(attributionSource, sessionHandle,
                 rangingPreference, callbacks);
     }
