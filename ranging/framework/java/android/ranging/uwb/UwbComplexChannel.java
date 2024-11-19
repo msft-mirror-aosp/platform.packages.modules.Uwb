8f67b4b2c4a4de708bada5fcf0e4f2c9dc2e6e40
Shreshta Manu <shreshtabm@google.com>
2024-11-19 06:16:20 +0000

    [Ranging] Remove @see tag
    Bug: 379696274
    Test: Compiles
    Change-Id: I913100edaac7010add378aa3263417c25ab562bf

--- ranging/framework/java/android/ranging/uwb/UwbComplexChannel.java
+++ ranging/framework/java/android/ranging/uwb/UwbComplexChannel.java
@@ -164,7 +164,7 @@ public final class UwbComplexChannel implements Parcelable {
      *
      * @return The preamble index, which is one of the predefined UWB preamble indices:
      *
-     * @See <a href="https://groups.firaconsortium.org/wg/members/document/1949> FiRa UCI Spec.</a>
+     * See <a href="https://groups.firaconsortium.org/wg/members/document/1949> FiRa UCI Spec.</a>
      */
 
     @UwbPreambleCodeIndex
