323947f990331e3e6deb34a3d67e56d572cf7535
Cole Faust <colefaust@google.com>
2025-01-31 15:35:07 -0800

    Replace .toList() with .collect()
    In android, .toList() was only introduced in API level 34, but this
    module has a min_sdk_version of 33. Use .collect() instead, which
    also allows us to collect directly to a set and avoid a copy.
    
    This was found when updating android lint, the old version of the
    NewApi linter missed this.
    
    Bug: 383061307
    Test: Presubmits
    Change-Id: I80ea2ef142b57443994ea4d04ad88e49183eb15d

--- ranging/service/java/com/android/server/ranging/uwb/UwbConfigSelector.java
+++ ranging/service/java/com/android/server/ranging/uwb/UwbConfigSelector.java
@@ -64,6 +64,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /** Selects a {@link UwbConfig} from local and peer device capabilities */
@@ -71,10 +72,9 @@ public class UwbConfigSelector {
     private static final String TAG = UwbConfigSelector.class.getSimpleName();
 
     private static final Set<@UwbComplexChannel.UwbPreambleCodeIndex Integer> HPRF_INDEXES =
-            Set.copyOf(
-                    IntStream.rangeClosed(UWB_PREAMBLE_CODE_INDEX_25, UWB_PREAMBLE_CODE_INDEX_32)
-                            .boxed()
-                            .toList());
+            IntStream.rangeClosed(UWB_PREAMBLE_CODE_INDEX_25, UWB_PREAMBLE_CODE_INDEX_32)
+                        .boxed()
+                        .collect(Collectors.toSet());
 
     private final SessionConfig mSessionConfig;
     private final OobInitiatorRangingConfig mOobConfig;
